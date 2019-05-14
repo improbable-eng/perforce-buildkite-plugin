90e58dd433b7909da8eb67a087e77ecefde25c98
Carl Johnson <carljohnson@improbable.io>
2019-05-14 11:28:23 +0100

    comment why == local

--- python/checkout.py
+++ python/checkout.py
@@ -8,7 +8,8 @@ import subprocess
 import perforce
 
 __ACCESS_TOKEN__ = os.environ['BUILDKITE_AGENT_ACCESS_TOKEN']
-__LOCAL_RUN__ = os.environ['BUILDKITE_AGENT_NAME'] == 'local'
+# https://github.com/buildkite/cli/blob/e8aac4bedf34cd8084a3ae7a4ab7812c611d0310/local/run.go#L403
+__LOCAL_RUN__ = os.environ['BUILDKITE_AGENT_NAME'] == 'local' 
 __REVISION_METADATA__ = 'buildkite:perforce:revision'
 
 def get_build_revision():
