105af22e9bdee7910cc7b86d82ceac4e0a6434f3
Carl Johnson <carljohnson@improbable.io>
2019-06-04 13:51:51 +0100

    Save a copy of shelved changelists to run buildkite builds against (#63)

--- python/buildkite.py
+++ python/buildkite.py
@@ -3,12 +3,16 @@ Interact with buildkite as part of plugin hooks
 """
 import os
 import subprocess
+from datetime import datetime
 
 __ACCESS_TOKEN__ = os.environ['BUILDKITE_AGENT_ACCESS_TOKEN']
 # https://github.com/buildkite/cli/blob/e8aac4bedf34cd8084a3ae7a4ab7812c611d0310/local/run.go#L403
 __LOCAL_RUN__ = os.environ['BUILDKITE_AGENT_NAME'] == 'local'
+
 __REVISION_METADATA__ = 'buildkite:perforce:revision'
 __REVISION_ANNOTATION__ = "Revision: %s"
+__SHELVED_METADATA__ = 'buildkite:perforce:shelved'
+__SHELVED_ANNOTATION__ = "[%(timestamp)s] Saved shelved change %(original)s as %(copy)s"
 
 def get_env():
     """Get env vars passed in via plugin config"""
@@ -36,13 +40,6 @@ def get_config():
     conf['view'] = ['%s %s' % (v, next(view_iter)) for v in view_iter]
     return conf
 
-def get_shelved_change():
-    """Get a shelved changelist that will be patched into each workspace"""
-    branch = os.environ.get('BUILDKITE_BRANCH', '')
-    if branch.isdigit():
-        return branch
-    return None
-
 def get_metadata(key):
     """If it exists, retrieve metadata from buildkite for a given key"""
     if not __ACCESS_TOKEN__ or __LOCAL_RUN__:
@@ -61,6 +58,29 @@ def set_metadata(key, value, overwrite=False):
     if overwrite or subprocess.call(['buildkite-agent', 'meta-data', 'exists', key]) == 100:
         subprocess.call(['buildkite-agent', 'meta-data', 'set',  key, value])
         return True
+
+def get_users_changelist():
+    """Get the shelved changelist supplied by the user, if applicable"""
+    branch = os.environ.get('BUILDKITE_BRANCH', '')
+    if branch.isdigit():
+        return branch
+
+def get_build_changelist():
+    """Get a saved version of the users originally supplied changelist, if available"""
+    return get_metadata(__SHELVED_METADATA__)
+
+def set_build_changelist(changelist):
+    """Set a shelved change that should be used instead of the user-supplied one"""
+    if set_metadata(__SHELVED_METADATA__, changelist):
+        subprocess.call([
+            'buildkite-agent', 'annotate', 
+            __SHELVED_ANNOTATION__.format(**{
+                'timestamp': datetime.now().strftime("%m/%d/%Y %H:%M:%S"),
+                'original': get_users_changelist(),
+                'copy': changelist,
+            }),
+            '--context', __SHELVED_METADATA__
+        ])
 
 def get_build_revision():
     """Get a p4 revision for the build to sync to"""
