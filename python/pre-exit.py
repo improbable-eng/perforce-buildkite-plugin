1222a9a3655d0da2234784f87a9a58fe7ae59842
Carl Johnson <carljohnson@improbable.io>
2019-08-07 10:59:21 +0100

    unused imports

--- python/pre-exit.py
+++ python/pre-exit.py
@@ -2,13 +2,9 @@
 Entrypoint for pre-exit hook
 """
 import os
-import argparse
-import subprocess
-import re
 
 from perforce import P4Repo
 from buildkite import get_env, get_config
-
 
 def main():
     """Perform any last-second cleanup regardless of success or failure"""
