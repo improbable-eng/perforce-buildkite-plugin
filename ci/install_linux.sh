28c2089a012cdcd6068a9ae505c1746e1ee21b9a
Carl Johnson <carljohnson@improbable.io>
2019-09-30 14:32:15 +0100

    Fix build (#125)
    * fix warnings
    
    * update p4api ver

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -9,4 +9,4 @@ wget http://www.perforce.com/downloads/perforce/r18.2/bin.linux26x86_64/p4api.tg
 tar xzf p4api.tgz --directory /tmp/
 wget https://files.pythonhosted.org/packages/36/5a/0a1b192cdecd31cb8bc0d0ba39c73ffd84ce823053d0004823a1fdbe1440/p4python-2018.2.1743033.tar.gz
 tar xfz p4python-2018.2.1743033.tar.gz --directory /tmp/
-pushd /tmp/p4python-2018.2.1743033/ && python setup.py install --apidir /tmp/p4api-2018.2.1751184 && popd
+pushd /tmp/p4python-2018.2.1743033/ && python setup.py install --apidir /tmp/p4api-2018.2.1850148 && popd
