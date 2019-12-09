2953491845c844b678b0e9adda0b4912c27167a7
Carl Johnson <carljohnson@improbable.io>
2019-12-09 12:01:43 +0000

    Fix unit tests vs new p4api minor version release (#134)

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -6,7 +6,9 @@ wget http://www.perforce.com/downloads/perforce/r18.2/bin.linux26x86_64/p4d && s
 
 # Build P4Python from source, pip install fails as we cannot connect to ftp.perforce.com from travis agents
 wget http://www.perforce.com/downloads/perforce/r18.2/bin.linux26x86_64/p4api.tgz
+# Detect concrete version number inside p4api.tgz
+P4API_VERSION=$(tar -tf p4api.tgz | head -1)
 tar xzf p4api.tgz --directory /tmp/
 wget https://files.pythonhosted.org/packages/36/5a/0a1b192cdecd31cb8bc0d0ba39c73ffd84ce823053d0004823a1fdbe1440/p4python-2018.2.1743033.tar.gz
 tar xfz p4python-2018.2.1743033.tar.gz --directory /tmp/
-pushd /tmp/p4python-2018.2.1743033/ && python setup.py install --apidir /tmp/p4api-2018.2.1850148 && popd
+pushd /tmp/p4python-2018.2.1743033/ && python setup.py install --apidir /tmp/${P4API_VERSION} && popd
