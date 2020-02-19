663e3d7c24298657959a76430ef70553c6d83ab0
Carl Johnson <carljohnson@improbable.io>
2020-02-19 15:13:05 +0000

    Accept all types of revision specifiers (#152)

--- python/checkout.py
+++ python/checkout.py
@@ -4,7 +4,6 @@ Entrypoint for checkout hook
 import os
 import argparse
 import subprocess
-import re
 
 from perforce import P4Repo
 from buildkite import (get_env, get_config, get_build_revision, set_build_revision,
@@ -19,14 +18,9 @@ def main():
     repo = P4Repo(**config)
 
     revision = get_build_revision()
-    if not revision.isdigit():
-        # Resolve 'HEAD' or ignore git sha and find a concrete revision
+    if revision is None:
         revision = repo.head()
         set_build_revision(revision)
-
-    # Convert changelist number to revision specifier
-    if re.match(r'^\d*$', revision):
-        revision = '@%s' % revision
 
     repo.sync(revision=revision)
 
