2a7e8904a5920506a4722fa36ff27310809a7ef1
Carl Johnson <carljohnson@improbable.io>
2020-02-25 13:00:48 +0000

    suppress SIGPIPE error

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -10,7 +10,7 @@ sudo mv p4d /usr/local/bin/p4d
 # Build P4Python from source, pip install fails as we cannot connect to ftp.perforce.com from travis agents
 wget http://www.perforce.com/downloads/perforce/r18.2/bin.linux26x86_64/p4api.tgz
 # Detect concrete version number inside p4api.tgz, changes as new versions are published
-P4API_VERSION=$(tar -tf p4api.tgz | head -1)
+P4API_VERSION=$(tar -tf p4api.tgz | head -1 || true)
 tar xzf p4api.tgz --directory /tmp/
 wget https://files.pythonhosted.org/packages/36/5a/0a1b192cdecd31cb8bc0d0ba39c73ffd84ce823053d0004823a1fdbe1440/p4python-2018.2.1743033.tar.gz
 tar xfz p4python-2018.2.1743033.tar.gz --directory /tmp/
