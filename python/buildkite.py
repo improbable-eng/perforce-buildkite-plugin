30e9fe919e0596a706caf9d3714bcbbacdb814f4
Carl Johnson <carljohnson@improbable.io>
2020-06-29 12:54:40 +0100

    ws

--- python/buildkite.py
+++ python/buildkite.py
@@ -110,14 +110,13 @@ def get_build_revision():
     # Convert bare changelist number to revision specifier
     # Note: Theoretically, its possible for all 40 characters of a git sha to be digits.
     #       In practice, the inconvenience of forcing users to always include '@' outweighs this risk (~1 in 7 billion)
-
     if revision.isdigit():
         revision = '@%s' % revision
     # Filter to only valid revision specifiers
     if revision.startswith('@') or revision.startswith('#'):
         return revision
     # Unable to establish a concrete revision for the build
-    return None
+    return None 
 
 def set_build_revision(revision):
     """Set the p4 revision for following jobs in this build"""
