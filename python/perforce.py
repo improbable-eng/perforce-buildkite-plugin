bac97af0eced078bbf44037c63f0ca7527491689
Joshua Huburn <joshuahuburn@improbable.io>
2022-05-05 16:31:24 +0100

    Hack to remove bad tickets

--- python/perforce.py
+++ python/perforce.py
@@ -66,6 +66,26 @@ class P4Repo:
         else:
             print("p4config doesn't exist on disk")
 
+        # Hack to remove bad ticket 
+        userdir = Path.home()
+        print(f"User dir is {userdir}")
+        p4ticket = Path(userdir, "p4tickets.txt")
+        print(f"p4 ticket is at {p4ticket}")
+        if os.path.exists(p4ticket):
+            print("Found p4 tickets, removing bad ticket")
+            os.chmod( p4ticket, stat.S_IWRITE )
+            with open(p4ticket, "r") as f:
+                lines = f.readlines()
+            with open(p4ticket, "w") as f:
+                for line in lines:
+                    if "EF94E790EEA5112D" not in line:
+                        f.write(line)
+                    else:
+                        print("Found bad ticket. Removing.")
+        else :
+            print("Found p4 tickets not found, uh oh")
+
+
         self.perforce.connect()
 
         if self.perforce.port.startswith('ssl'):
